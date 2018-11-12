bdb380e1467e85ce551d0aeae4ce4c0bce2108cd
xinyanhe <xinyanhexinyan@gmail.com>
2018-11-12 22:41:44 +0800

    add ok

--- inference/cc/run_infer.cc
+++ inference/cc/run_infer.cc
@@ -1,5 +1,6 @@
 // 2018, Patrick Wieschollek <mail@patwie.com>
 #include <tensorflow/core/protobuf/meta_graph.pb.h>
+#include <tensorflow/core/graph/graph.h>
 #include <tensorflow/core/public/session.h>
 #include <tensorflow/core/public/session_options.h>
 #include "tensorflow/cc/ops/image_ops.h"
@@ -8,7 +9,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-
+#include <chrono>
 typedef std::vector<std::pair<std::string, tensorflow::Tensor>> tensor_dict;
 
 /**
@@ -38,35 +39,22 @@ tensorflow::Status LoadModel(tensorflow::Session *sess, std::string graph_fn,
   tensorflow::Status status;
 
   // Read in the protobuf graph we exported
-  tensorflow::MetaGraphDef graph_def;
+  tensorflow::GraphDef graph_def;
+  //tensorflow::MetaGraphDef graph_def;
   status = ReadBinaryProto(tensorflow::Env::Default(), graph_fn, &graph_def);
   if (status != tensorflow::Status::OK()) return status;
 
   // create the graph in the current session
-  status = sess->Create(graph_def.graph_def());
+  status = sess->Create(graph_def);
   if (status != tensorflow::Status::OK()) return status;
 
-  // restore model from checkpoint, iff checkpoint is given
-  if (checkpoint_fn != "") {
-    const std::string restore_op_name = graph_def.saver_def().restore_op_name();
-    const std::string filename_tensor_name =
-        graph_def.saver_def().filename_tensor_name();
-
-    tensorflow::Tensor filename_tensor(tensorflow::DT_STRING,
-                                       tensorflow::TensorShape());
-    filename_tensor.scalar<std::string>()() = checkpoint_fn;
-
-    tensor_dict feed_dict = {{filename_tensor_name, filename_tensor}};
-    status = sess->Run(feed_dict, {}, {restore_op_name}, nullptr);
-    if (status != tensorflow::Status::OK()) return status;
-  } else {
-    // virtual Status Run(const std::vector<std::pair<string, Tensor> >& inputs,
-    //                  const std::vector<string>& output_tensor_names,
-    //                  const std::vector<string>& target_node_names,
-    //                  std::vector<Tensor>* outputs) = 0;
-    status = sess->Run({}, {}, {"init"}, nullptr);
-    if (status != tensorflow::Status::OK()) return status;
-  }
+  // virtual Status Run(const std::vector<std::pair<string, Tensor> >& inputs,
+  //                  const std::vector<string>& output_tensor_names,
+  //                  const std::vector<string>& target_node_names,
+  //                  std::vector<Tensor>* outputs) = 0;
+  status = sess->Run({}, {}, {"init"}, nullptr);
+  if (status != tensorflow::Status::OK()) return status;
+  
 
   return tensorflow::Status::OK();
 }
@@ -90,7 +78,7 @@ int main(int argc, char const *argv[]) {
   //for (int i = 0; i < 2; ++i) data_[i] = 1;
 
   // read image in OpenCV
-  std::string fn = "Grace_Hopper.png";
+  std::string fn = "../p.png";
   cv::Mat image;
   image = cv::imread(fn);
   if (!image.data ) {
@@ -108,36 +96,39 @@ int main(int argc, char const *argv[]) {
   // copy data from OpenCv to TensorFlow Tensor
   std::copy_n((char*) image_float_data, image_shape.num_elements() * sizeof(float),
                   const_cast<char*>(image_tensor.tensor_data().data()));
-  /*
-    upsample_size = [int(self.target_size[1] / 8 * upsample_size), int(self.target_size[0] / 8 * upsample_size)]
-    self.upsample_size = self.graph.get_tensor_by_name('output_tensors/upsample_size:0')
-    self.tensor_heatMat_up = self.graph.get_tensor_by_name('output_tensors/upsample_heatmat:0')
-    self.tensor_pafMat_up = self.graph.get_tensor_by_name('output_tensors/upsample_pafmat:0')
-    self.tensor_peaks = self.graph.get_tensor_by_name('output_tensors/tensor_peaks:0')
-  */
+
   tensorflow::TensorShape shape({2});
-  tensorflow::Tensor upsample_tensor(tensorflow::DT_FLOAT, shape);
+  tensorflow::Tensor upsample_tensor(tensorflow::DT_INT32, shape);
   // same as in python file
-  auto data_ = upsample_tensor.flat<float>().data();
+  auto data_ = upsample_tensor.flat<int>().data();
   for (int i = 0; i < 2; ++i) data_[i] = int(224/8.0*4);
   tensor_dict feed_dict = {
-      {"image:0", image_tensor}, 
+      {"TfPoseEstimator/image:0", image_tensor}, 
       {"output_tensors/upsample_size:0",upsample_tensor},
   };
-  std::vector<tensorflow::Tensor> outputs;
-  TF_CHECK_OK(sess->Run(feed_dict, 
-                        {"output_tensors/upsample_heatmat:0", 
-                        "output_tensors/upsample_pafmat:0", 
-                        "output_tensors/tensor_peaks:0"},
-                        {}, &outputs));
-  //auto upsample_heatmat = outputs[0].tensor();//Eigen::tensor
-  //auto upsample_pafmat  = outputs[1].tensor();//Eigen::tensor
-  //auto tensor_peaks     = outputs[2].tensor();//Eigen::tensor
-
+  while(true)
+  {
+    std::vector<tensorflow::Tensor> outputs;
+    auto start = std::chrono::steady_clock::now();
+    sess->Run(feed_dict, 
+                      {"output_tensors/upsample_heatmat:0", 
+                      "output_tensors/upsample_pafmat:0", 
+                      "output_tensors/tensor_peaks:0"},
+                      {}, &outputs);
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "input                 " << image_tensor.DebugString() << std::endl;
+    std::cout << "upsample_heatmat" << outputs[0].DebugString() << std::endl;
+    std::cout << "upsample_pafmat" << outputs[1].DebugString() << std::endl;
+    std::cout << "tensor_peaks" << outputs[2].DebugString() << std::endl;
+    auto diff = end - start;
+    std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
+  }
+  /*
   std::cout << "input                 " << image_tensor.DebugString() << std::endl;
   std::cout << "upsample_heatmat" << outputs[0].DebugString() << std::endl;
   std::cout << "upsample_pafmat" << outputs[1].DebugString() << std::endl;
   std::cout << "tensor_peaks" << outputs[2].DebugString() << std::endl;
+  */
  
   return 0;
 }
